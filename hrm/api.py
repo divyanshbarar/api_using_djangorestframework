6886fa8a0048e46c56bf17f78ed0229dbcbded4e
Divyansh Barar <divyanshbarar@gmail.com>
2020-11-17 01:11:34 +0530

    first commit

---
+++ hrm/api.py
@@ -0,0 +1,39 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+
+class UserList(APIView):
+  def get(self,request):
+    model=User.objects.all()
+    serializers=UsersSerializers(model,many=True)
+    return Response(serializers.data)
+
+  def post(self,request):
+    serializer=UsersSerializers(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetail(APIView):
+  def get(self,request,employee_id):
+    try:
+      model=User.objects.get(id=employee_id)
+    except User.DoesNotExist:
+      return Response(f"User with {employee_id} is not found in database",status=status.HTTP_404_NOT_FOUND)
+    serializers=UsersSerializers(model)
+    return Response(serializers.data)
+
+  def put(self,request,employee_id):
+    try:
+      model=User.objects.get(id=employee_id)
+    except User.DoesNotExist:
+      return Response(f"User with {employee_id} is not found in database",status=status.HTTP_404_NOT_FOUND)
+    serializer=UsersSerializers(model,data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+       
---
+++ hrm/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    employee_id=models.CharField(max_length=10,unique=True)
+    name=models.CharField(max_length=29)
+    age=models.IntegerField()
+    ranking=models.FloatField()
+    
+    def upload_photo(self,filename):
+        path='hrm/photo/{}'.format(filename)
+        return path
+
+    photo=models.ImageField(upload_to=upload_photo,null=True,blank=True)
+
+    def upload_file(self,filename):
+        path='hrm/file/{}'.format(filename)
+        return path
+
+    resume=models.ImageField(upload_to=upload_file,null=True,blank=True)
+
+    def __str__(self):
+       return self.name
