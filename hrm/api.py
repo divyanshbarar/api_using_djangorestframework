16f8293f3d424ac4b8a24f52aa4475d92113c9e3
Divyansh Barar <divyanshbarar@gmail.com>
2020-11-18 01:17:38 +0530

    second commit

--- hrm/api.py
+++ hrm/api.py
@@ -36,4 +36,11 @@ class UserDetail(APIView):
       serializer.save()
       return Response(serializer.data,status=status.HTTP_201_CREATED)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-       
+
+  def delete(self,request,employee_id):
+    try:
+      model=User.objects.get(id=employee_id)
+    except User.DoesNotExist:
+      return Response(f"User with {employee_id} is not found in database",status=status.HTTP_404_NOT_FOUND)
+    model.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
