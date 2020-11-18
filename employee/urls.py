57ba6a6fe5949bc6c17a314b0fad28b7842b5ba2
Divyansh Barar <divyanshbarar@gmail.com>
2020-11-18 18:15:13 +0530

    final commit

--- employee/urls.py
+++ employee/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/users_list/$',UserList.as_view(),name='user_list'),
     url(r'^api/users_list/(?P<employee_id>\d+)/$',UserDetail.as_view(),name='user_list'),
+    url(r'^api/auth/$',UserAuthentication.as_view(),name='User Authentication Api'),
 ]
--- hrm/api.py
+++ hrm/api.py
@@ -1,7 +1,18 @@
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
+
+class UserAuthentication(ObtainAuthToken):
+  def post(self,request,*args,**kwargs):
+    serializer=self.serializer_class(data=request.data,context={'request':request})
+    serializer.is_valid(raise_exception=True)
+    user=serializer.validated_data['user']
+    token, created=Token.objects.get_or_create(user=user)
+    return Response(token.key)
+
 
 class UserList(APIView):
   def get(self,request):
